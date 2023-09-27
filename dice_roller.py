323f6d69dd8ccb1e21e438143e6a34a37cdb38a4
Andrew Magdurulan <59845713+andrewkmag@users.noreply.github.com>
2023-09-26 21:10:51 -0700

    Use join on input paremeters of roll command and refactor code

--- dice_roller.py
+++ dice_roller.py
@@ -14,9 +14,9 @@ intents.presences = True
 diceRollerBot = commands.Bot(command_prefix='/', intents=intents)
 
 # Define set to contain valid types of checks for command parameter
-CHECKTYPESET = {"athletics", "acrobatics", "sleightofhand",
+CHECKTYPESET = {"athletics", "acrobatics", "sleight of hand",
                 "stealth", "arcana", "history", "investigation",
-                "nature", "religion", "animalhandling",
+                "nature", "religion", "animal handling",
                 "insight", "medicine", "perception", "survival",
                 "deception",  "intimidation", "performance",
                 "persuasion"}
@@ -26,62 +26,39 @@ CHECKTYPESET = {"athletics", "acrobatics", "sleightofhand",
 async def on_ready():
     print(f'{diceRollerBot.user.name} ~Ready to perform checks ...')
 
-# Ability Check processing to handle checks that have more than one string
-# I.e. Ability Checks that are seperated with whitespace characters
-async def processArgs(*args):
-    temp: str = ""
-    # Traverse through the arguments
-    for i in args:
-        temp = " ".join(*args)
-        print(f'CONSOLE: Concatenated argument is: {temp}')
-    return temp
-
 # Bot command: Roll a d20 and check that value against a randomly generated number
 @diceRollerBot.command()
 async def roll(ctx, *args):
-    # Process arguments passed into coroutine that handles ability checks with whitespaces
-    processArgsResult = processArgs(*args)
-    checkType = await processArgsResult
-    print(f'CONSOLE: Final processed argument is: {checkType}')
-    
-    # Convert to lowercase and remove whitespaces
-    temp: str = checkType
-    checkTypeString = temp.replace(" ", "").lower()
-    print(f'CONSOLE: String used for checking {checkTypeString}')
-    
-    # Simulate the roll of a 20 sided dice
-    result: int = random.randint(1,20)
-    # Setup random number to perform check against
-    numberToPass: int = random.randint(2,20)
-    validAbilityCheck: bool = op.countOf(CHECKTYPESET, checkTypeString)
-    print(f'CONSOLE: Valid ability check result: {validAbilityCheck}')
+    # Use join to handle checks with more than one string
+    checkType = " ".join(args).lower()
 
     # Check that ability check type is valid
-    if op.countOf(CHECKTYPESET, checkTypeString) == True:
+    if op.countOf(CHECKTYPESET, checkType) == True:
+            
+        # Simulate the roll of a 20 sided dice
+        result: int = random.randint(1,20)
+        # Setup random number to perform check against
+        numberToPass: int = random.randint(2,20)
+        
         await ctx.send(f'Difficulty Class: {numberToPass}')
-        print(f'CONSOLE: Difficulty Class: {numberToPass}')
 
         # Handle special cases for rolls that are 1 or 20
         if result == 20:
             time.sleep(1) 
             await ctx.send(f"Rolled a {result}: CRITICAL SUCCESS")
-            print(f"CONSOLE: Rolled a {result}: CRITICAL SUCCESS")
             return
         elif result == 1:
             time.sleep(1) 
-            await ctx.send(f"Rolled a {result}: CRITICAL FAILURE")
-            print(f"CONSOLE: Rolled a {result}: CRITICAL FAILURE")
+            await ctx.send(f"Rolled a {result}: CRITICAL FAILURE")       
             return
         # Check if roll passed or failed against the generated number to beat
         elif result >= numberToPass:
             time.sleep(1) 
             await ctx.send(f'Rolled a {result}: SUCCESS')
-            print(f'CONSOLE: Rolled a {result}: SUCCESS')
             return
         elif result < numberToPass:
             time.sleep(1) 
             await ctx.send(f'Rolled a {result}: FAILURE')
-            print(f'CONSOLE: Rolled a {result}: FAILURE')
             return
     else:
         time.sleep(1) 
