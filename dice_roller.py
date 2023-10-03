66997358ec88fa862881dbd1a1f33b62378df50e
Andrew Magdurulan <59845713+andrewkmag@users.noreply.github.com>
2023-10-03 00:32:32 -0700

    Add custom help command and add more string to ability check set

--- dice_roller.py
+++ dice_roller.py
@@ -4,6 +4,7 @@ import random
 import operator as op
 import time
 
+
 intents = discord.Intents.default()
 intents.message_content = True
 intents.presences = True
@@ -13,20 +14,26 @@ intents.presences = True
 # Setup bot with command prefix '/'
 diceRollerBot = commands.Bot(command_prefix='/', intents=intents)
 
+# Remove provided help command
+diceRollerBot.remove_command('help')
+
 # Define set to contain valid types of checks for command parameter
 CHECKTYPESET = {"athletics", "acrobatics", "sleight of hand",
                 "stealth", "arcana", "history", "investigation",
                 "nature", "religion", "animal handling",
                 "insight", "medicine", "perception", "survival",
                 "deception",  "intimidation", "performance",
-                "persuasion"}
+                "persuasion", "strength", "wisdom", "intelligence", 
+                "dexterity", "charisma", "constitution"}
 
 # Bot event: When bot is ready send message
 @diceRollerBot.event
 async def on_ready():
     print(f'{diceRollerBot.user.name} ~Ready to perform checks ...')
 
-# Bot command: Roll a d20 and check that value against a randomly generated number
+# Bot command: /roll <ability_check>
+# Roll a d20 (randomly generated number between 1 and 20) 
+# and check that value against the Difficulty Class number
 @diceRollerBot.command()
 async def roll(ctx, *args):
     # Use join to handle checks with more than one string
@@ -61,5 +68,26 @@ async def roll(ctx, *args):
         await ctx.send(f"Invalid/Unknown ability check ...")
         await ctx.send(f"Please enter a valid ability check")
         return
+
+# Bot command: /help roll
+# When no arguments are specified list the possible commands to provide documentation about
+# If valid argument is specified detail the usage of said command
+@diceRollerBot.command()
+async def help(ctx, *args):
+    # Handle case of empty arguments
+    if len(args) == 0:
+        await ctx.send(f'```Documentation available for commands:\n- roll```')
+        return
+    
+    # Check which command to display documentation for
+    processCommandString: str = args[0].lower()
+    match processCommandString:
+        case "roll":
+            rollDocumentationString: str = '```Usage: /roll <ability_check>\nExample: /roll sleight of hand\nList of Valid Ability Checks:\n'
+            for x in CHECKTYPESET:
+                rollDocumentationString = rollDocumentationString + "-> " + x + "\n"
+            await ctx.send(f'{rollDocumentationString}```')
+        case _:
+            await ctx.send(f'No documentation provided for the command / non-existant command input')
 
 diceRollerBot.run('')
